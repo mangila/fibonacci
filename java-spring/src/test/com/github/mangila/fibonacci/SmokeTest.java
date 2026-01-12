adde93fcea90a5556b2c109b7eed5bc3ac4ff8f4
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-12 23:38:42 +0100

    smoketest

---
+++ java-spring/src/test/com/github/mangila/fibonacci/SmokeTest.java
@@ -0,0 +1,13 @@
+package com.github.mangila.fibonacci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SmokeTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
