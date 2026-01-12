bd03814781cbb97a0516a4e4e17ec6845e9a7952
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-12 23:12:32 +0100

    dynamic test

--- java-spring/src/test/com/github/mangila/fibonacci/FibonacciCalculatorTest.java
+++ java-spring/src/test/com/github/mangila/fibonacci/FibonacciCalculatorTest.java
@@ -1,10 +1,14 @@
 package com.github.mangila.fibonacci;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.util.StopWatch;
 
 import java.math.BigInteger;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,6 +21,22 @@ class FibonacciCalculatorTest {
     private static final int ONE_HUNDRED_THOUSAND = 100_000;
     private static final int FIVE_HUNDRED_THOUSAND = 500_000;
     private static final int ONE_MILLION = 1_000_000;
+
+    @TestFactory
+    Collection<DynamicTest> fibonacciNumbers() {
+        return Stream.of(HUNDRED, THOUSAND, TEN_THOUSAND, ONE_HUNDRED_THOUSAND, FIVE_HUNDRED_THOUSAND, ONE_HUNDRED_THOUSAND)
+                .map(testInt -> DynamicTest.dynamicTest("Fibonacci number " + testInt, () -> {
+                    var stopWatch = new StopWatch();
+                    stopWatch.start("iterative");
+                    BigInteger i = FibonacciCalculator.iterative(testInt);
+                    stopWatch.stop();
+                    stopWatch.start("fastDoubling");
+                    BigInteger f = FibonacciCalculator.fastDoubling(testInt);
+                    stopWatch.stop();
+                    System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
+                    assertThat(i).isEqualTo(f);
+                })).toList();
+    }
 
     @Test
     void TEN() {
@@ -35,89 +55,5 @@ class FibonacciCalculatorTest {
         assertThat(r)
                 .isEqualTo(i)
                 .isEqualTo(f);
-    }
-
-    @Test
-    void HUNDRED() {
-        int index = HUNDRED;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
-    }
-
-    @Test
-    void THOUSAND() {
-        int index = THOUSAND;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
-    }
-
-    @Test
-    void TEN_THOUSAND() {
-        int index = TEN_THOUSAND;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
-    }
-
-    @Test
-    void ONE_HUNDRED_THOUSAND() {
-        int index = ONE_HUNDRED_THOUSAND;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
-    }
-
-    @Test
-    void FIVE_HUNDRED_THOUSAND() {
-        int index = FIVE_HUNDRED_THOUSAND;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
-    }
-
-    @Test
-    void ONE_MILLION() {
-        int index = ONE_MILLION;
-        var stopWatch = new StopWatch();
-        stopWatch.start("iterative");
-        BigInteger i = FibonacciCalculator.iterative(index);
-        stopWatch.stop();
-        stopWatch.start("fastDoubling");
-        BigInteger f = FibonacciCalculator.fastDoubling(index);
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
-        assertThat(i).isEqualTo(f);
     }
 }
