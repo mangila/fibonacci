ced077bd1a0236ec1c75c817473789ac2e15b5bf
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-19 00:00:57 +0100

    class -> className

--- client/src/pages/_components/DatasourceRadio.tsx
+++ client/src/pages/_components/DatasourceRadio.tsx
@@ -3,16 +3,16 @@ const DatasourceRadio = () => {
     <>
       <form
         id="source-form"
-        class="flex justify-center space-x-4 mt-6 text-xl gap-4 mb-4"
+        className="flex justify-center space-x-4 mt-6 text-xl gap-4 mb-4"
       >
         <input
-          class="btn w-32 h-16"
+          className="btn w-32 h-16"
           type="radio"
           name="data-source"
           aria-label="Livestream"
         />
         <input
-          class="btn w-32 h-16"
+          className="btn w-32 h-16"
           type="radio"
           name="data-source"
           aria-label="Query"
