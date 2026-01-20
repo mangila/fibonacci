1414951ca53209d67ab94f8bf8e4e66fa4259ab4
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-20 19:10:27 +0100

    update h2c script

--- http/h2c.sh
+++ http/h2c.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cowsay "Test script for Fibonacci Backend with H2C, (HTTP2 without TLS)"
 curl --http2 -i -N "http://localhost:8080/api/v1/sse/livestream/mangila-livestream?streamKey=helloworld"
