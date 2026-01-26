48c027a671a5c9ba367c2ecd1e912bd77180537c
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-26 15:26:03 +0100

    stream to client

--- java/flyway/postgresql/V1.0__create_table_fibonacci_results.sql
+++ java/flyway/postgresql/V1.0__create_table_fibonacci_results.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS fibonacci_results
     id        SERIAL PRIMARY KEY,
     sequence  INT UNIQUE NOT NULL,
     -- If doing huge fibonacci sequences, this could overflow, but we're not doing that here.
-    -- but should be a BYTEA then when fib sequences are over 500k ish'
+    -- for fibonacci sequences around 500k ish, we should use BYTEA
     result    NUMERIC    NOT NULL,
     precision INT        NOT NULL
 );
