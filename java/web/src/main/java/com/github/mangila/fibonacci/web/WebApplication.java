503116b2e2430b673d242482fe7e99b00aa2e3fd
Erik Olsson <olsson.erik1993@gmail.com>
2026-01-26 15:31:58 +0100

    stream to client

--- java/web/src/main/java/com/github/mangila/fibonacci/web/WebApplication.java
+++ java/web/src/main/java/com/github/mangila/fibonacci/web/WebApplication.java
@@ -2,6 +2,12 @@ package com.github.mangila.fibonacci.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+
+import java.util.TreeMap;
 
 @SpringBootApplication
 public class WebApplication {
@@ -10,4 +16,22 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
+    @EventListener
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        ConfigurableEnvironment env = (ConfigurableEnvironment) event.getApplicationContext().getEnvironment();
+
+        System.out.println("========= ALL RESOLVED PROPERTIES =========");
+
+        TreeMap<String, Object> props = new TreeMap<>();
+        env.getPropertySources().forEach(source -> {
+            if (source instanceof EnumerablePropertySource) {
+                for (String name : ((EnumerablePropertySource<?>) source).getPropertyNames()) {
+                    props.put(name, env.getProperty(name));
+                }
+            }
+        });
+
+        props.forEach((key, value) -> System.out.printf("%s = %s%n", key, value));
+        System.out.println("===========================================");
+    }
 }
